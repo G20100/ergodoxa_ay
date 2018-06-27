dbd54890470a8256a0a591f7615d34f93c8a1ee3
aycoffee <akifumi@yoshikawa.in>
2018-06-27 09:11:23 +0900

    commit

--- keyboards/ergodoxay/keymaps/default/keymap.c
+++ keyboards/ergodoxay/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LALT,KC_LGUI,KC_FN1,
+        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LALT,KC_LGUI,TG(MDIA),
                                                     KC_PSCR,  KC_DEL,
                                                     KC_SPC,   KC_HOME,
         // right hand
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                          KC_FN1, TG(SYMB), KC_LEFT,KC_UP,KC_RGHT,          KC_FN1,
+                          KC_FN1, TG(SYMB), KC_LEFT,KC_UP,KC_RGHT,         TG(MDIA),
              KC_DEL,      KC_ESC,
              KC_END,	  KC_BSPC,					KC_DOWN
     ),
