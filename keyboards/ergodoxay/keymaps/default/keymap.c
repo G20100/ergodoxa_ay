517860997792a697786af6f35c562d7987360967
G20_100 <akifumi@yoshikawa.in>
2018-06-27 15:13:03 +0900

    marge

--- keyboards/ergodoxay/keymaps/default/keymap.c
+++ keyboards/ergodoxay/keymaps/default/keymap.c
@@ -41,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_C),
         KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LALT,KC_LGUI,TG(MDIA),
+        KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_GRV),
+        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LALT,MO(MDIA),MO(SYMB),
                                                     KC_PSCR,  KC_DEL,
                                                     KC_SPC,   KC_HOME,
         // right hand
              KC_6,  	  KC_7,   KC_8,   KC_9,   KC_0,  KC_MINS, 		   KC_EQL,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+             LCTL(KC_V),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                          KC_FN1, TG(SYMB), KC_LEFT,KC_UP,KC_RGHT,         TG(MDIA),
+             LALT(KC_GRV),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                          KC_FN1, TG(SYMB), KC_LEFT,KC_UP,KC_RGHT,         MO(MDIA),
              KC_DEL,      KC_ESC,
              KC_END,	  KC_BSPC,					KC_DOWN
     ),
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 [MDIA] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, SEND_STRING("] - ["),
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_TRNS,
