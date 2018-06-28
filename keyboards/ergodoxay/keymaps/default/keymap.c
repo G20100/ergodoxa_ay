7278d2931389d3dd5dc535b5c0f188b9f0a366ff
G20_100 <akifumi@yoshikawa.in>
2018-06-28 20:16:41 +0900

    ok

--- keyboards/ergodoxay/keymaps/default/keymap.c
+++ keyboards/ergodoxay/keymaps/default/keymap.c
@@ -44,17 +44,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_C),
         KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_GRV),
-        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LALT,MO(MDIA),MO(SYMB),
-                                                    KC_PSCR,  KC_DEL,
-                                                    KC_SPC,   KC_HOME,
+        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LGUI,MO(SYMB),KC_LSFT,
+                                                    KC_PSCR,  KC_HOME,
+                                                    KC_SPC,   KC_DEL,
         // right hand
              KC_6,  	  KC_7,   KC_8,   KC_9,   KC_0,  KC_MINS, 		   KC_EQL,
              LCTL(KC_V),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
              LALT(KC_GRV),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                          KC_FN1, TG(SYMB), KC_LEFT,KC_UP,KC_RGHT,         MO(MDIA),
-             KC_DEL,      KC_ESC,
-             KC_END,	  KC_BSPC,					KC_DOWN
+                          KC_RSFT, MO(SYMB), KC_LEFT,KC_UP,KC_RGHT,         MO(MDIA),
+             KC_END,      KC_ESC,
+             KC_BSPC,	  KC_ENT,					KC_DOWN
     ),
 /* Keymap 1: Symbol Layer
  *
