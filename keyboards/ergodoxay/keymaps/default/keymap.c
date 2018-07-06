7d5838451595027df432fb9f869af3122b89b193
G20_100 <akifumi@yoshikawa.in>
2018-07-06 12:18:22 +0900

    test

--- keyboards/ergodoxay/keymaps/default/keymap.c
+++ keyboards/ergodoxay/keymaps/default/keymap.c
@@ -41,20 +41,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_C),
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_V),
         KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_GRV),
-        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LGUI,MO(SYMB),KC_LSFT,
-                                                    KC_PSCR,  KC_HOME,
-                                                    KC_SPC,   KC_DEL,
+        KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_C),
+        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LGUI,MO(SYMB),KC_SPC,
+                                                    KC_PSCR,  KC_PGUP,
+                                                    KC_DEL,   KC_HOME,
         // right hand
              KC_6,  	  KC_7,   KC_8,   KC_9,   KC_0,  KC_MINS, 		   KC_EQL,
              LCTL(KC_V),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
              LALT(KC_GRV),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                          KC_RSFT, MO(SYMB), KC_LEFT,KC_UP,KC_RGHT,         MO(MDIA),
-             KC_END,      KC_ESC,
-             KC_BSPC,	  KC_ENT,					KC_DOWN
+                          KC_ENT, MO(SYMB), KC_LEFT,KC_UP,KC_RGHT,         MO(MDIA),
+             KC_PGDN,      KC_ESC,
+             KC_END,	  KC_BSPC,					KC_DOWN
     ),
 /* Keymap 1: Symbol Layer
  *
--- keyboards/ergodoxay/keymaps/withpoint/keymap.c
+++ keyboards/ergodoxay/keymaps/withpoint/keymap.c
@@ -41,19 +41,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_V),
         KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LALT,KC_LGUI,KC_FN1,
-                                                    KC_PSCR,  KC_DEL,
-                                                    KC_SPC,   KC_HOME,
+        KC_LSFT,        KC_Z,  		KC_X,   KC_C,   KC_V,   KC_B,   LALT(KC_C),
+        KC_LCTRL,		KC_LGUI,   	KC_LALT,KC_LGUI,MO(SYMB),KC_SPC,
+                                                    KC_PSCR,  KC_PGUP,
+                                                    KC_DEL,   KC_HOME,
         // right hand
              KC_6,  	  KC_7,   KC_8,   KC_9,   KC_0,  KC_MINS, 		   KC_EQL,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+             LCTL(KC_V),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,			KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                          KC_FN1, TG(SYMB), KC_LEFT,KC_UP,KC_RGHT,          KC_FN1,
-             KC_DEL,      KC_ESC,
+             LALT(KC_GRV),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                          KC_ENT, MO(SYMB), KC_LEFT,KC_UP,KC_RGHT,         MO(MDIA),
+             KC_PGDN,      KC_ESC,
              KC_END,	  KC_BSPC,					KC_DOWN
     ),
 /* Keymap 1: Symbol Layer
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 RGB_SLD, KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        RGB_TOG, KC_TRNS,
        KC_TRNS, RGB_HUD, RGB_HUI
-),	
+),
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
